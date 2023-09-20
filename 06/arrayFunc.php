5b6df40b0f813f5e5ae4bc96c8cc58288e901608
y.asano <tennis6293@gmail.com>
2023-09-20 17:16:40 +0900

    wip:05 id値

--- 06/arrayFunc.php
+++ 06/arrayFunc.php
@@ -3,6 +3,7 @@
 $domain = 'http://example.com?';
 $text = 'PHP MySQL Laravel';
 
-echo $domain .http_build_query([ '0'=> 'PHP', '1' => 'MySQL' , '2' => 'Laravel' ]);
+
+echo $domain .http_build_query(explode(' ' , $text));
 
 /* http://example.com?0=PHP&1=MySQL&2=Laravel */;
