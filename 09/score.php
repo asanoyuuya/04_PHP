145b2c1ceca93b383e58042b3cea8241c77a3f6b
y.asano <tennis6293@gmail.com>
2023-09-21 18:22:30 +0900

    add:09 select1

--- 09/score.php
+++ 09/score.php
@@ -4,24 +4,23 @@ $score = '';
 
 if (!empty($_POST)) {
     $score = $_POST['score'];
-}
 
-if (!is_numeric($score)) {
-    $result = '数値を入力してください';
-} elseif ($score < 0 or 100 < $score) {
-    $result = '不正な点数です';
-} elseif ($score == 100) {
-    $result = '満点おめでとう！';
-} elseif ($score >= 80) {
-    $result = '素晴らしいです！';
-} elseif ($score >= 60) {
-    $result = '合格です';
-} else {
-    $result = '不合格です';
+    if (!is_numeric($score)) {
+        $result = '数値を入力してください';
+    } elseif ($score < 0 or 100 < $score) {
+        $result = '不正な点数です';
+    } elseif ($score == 100) {
+        $result = '満点おめでとう！';
+    } elseif ($score >= 80) {
+        $result = '素晴らしいです！';
+    } elseif ($score >= 60) {
+        $result = '合格です';
+    } else {
+        $result = '不合格です';
+    }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -34,14 +33,16 @@ if (!is_numeric($score)) {
 
 <body>
     <h1>テスト結果判定</h1>
-    <form action="" method="post">
+    <form action="" method="post" novalidate>
         <p>点数：<input type="text" name="score" size="3" maxlength="3"
                 value="<?= htmlspecialchars($score, ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>">点
             <input type="submit" value="判定">
         </p>
     </form>
     <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
-        <p>判定：<?=$result?></p>
+        <p>判定：
+            <?= $result ?>
+        </p>
     <?php endif; ?>
 </body>
 
