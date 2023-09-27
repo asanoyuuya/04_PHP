719b20743adfc1c16ea8575453876e2d945804de
y.asano <tennis6293@gmail.com>
2023-09-27 12:54:31 +0900

    add:21 02_form

--- 21/02_form.php
+++ 21/02_form.php
@@ -1,3 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+$greeting = '';
+if (!empty($_POST)) {
+    $greeting = $_POST['greeting'];
+}
+
+
+/**
+ * XSS対策のサニタイジングと参照名省略
+ *
+ * @param string | null string
+ * @return string | null
+ *
+ */
+function h(?string $string): ?string
+{
+    if (empty($string)) return null;
+    return htmlspecialchars($string, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,8 +31,13 @@
 </head>
 <body>
     <form action="" method="post" novalidate>
-        <input type="text" name="">
+        <input type="text"  name="greeting" cols="50" value="<?= htmlspecialchars($greeting, ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>"></input>
         <p><input type="submit" value="送信"></p>
     </form>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+        <h2>
+            <?= h($greeting) ?>
+        </h2>
+    <?php endif; ?>
 </body>
 </html>
