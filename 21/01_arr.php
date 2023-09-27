a1d7fe6ba7c19e981f7017b38ad57230cf3d448a
y.asano <tennis6293@gmail.com>
2023-09-27 12:41:19 +0900

    update:01_arr 修正

--- 21/01_arr.php
+++ 21/01_arr.php
@@ -1,17 +1,13 @@
 <?php
 
 $editors = [
-    'VSCode' => 'MicroSoft',
+    'VSCode'   => 'MicroSoft',
     'PhpStorm' => 'JetBrains',
-    'Atom' => 'GitHub',
-    'Eclipse' => 'IBM',
-    'AEM' => 'Adobe',
+    'Atom'     => 'GitHub',
+    'Eclipse'  => 'IBM',
+    'AEM'      => 'Adobe'
 ];
 
-echo '<table>';
-foreach ($editors as $editor => $maker) {
-    echo '<tr>';
-    echo '<td>' . $editor . 'は' . $maker . '社開発です。</td>';
-    echo '</tr>';
+foreach ($editors as $tool => $maker) {
+    echo  $tool . 'は' . $maker . '社開発です。<br>';
 }
-echo '</table>';
---
+++ 21/02_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>挨拶</title>
+</head>
+<body>
+    <form action="" method="post" novalidate>
+        <input type="text" name="">
+        <p><input type="submit" value="送信"></p>
+    </form>
+</body>
+</html>
