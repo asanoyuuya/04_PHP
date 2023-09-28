d917fc3c679cd9198c9faf34d96ac2db46c914a8
y.asano <tennis6293@gmail.com>
2023-09-28 18:23:50 +0900

    add:21 15_ave

--- 21/15_ave.php
+++ 21/15_ave.php
@@ -1,8 +1,20 @@
 <?php
 
-$arrNms = [1, 7, 11];
+$arrNums = [1, 7, 11];
 
-function getAvg()
+/**
+ * 配列の平均値
+ *
+ * @param integer|array $arrNums
+ * @return integer
+ */
+function getAvg(int|array $arrNums): int
 {
-
+    $totalNums = 0;
+    foreach ($arrNums as $nums) {
+        $totalNums += $nums;
+    }
+    return round($totalNums / count($arrNums));
 }
+
+echo '平均値は' . getAvg($arrNums) . 'です。';
