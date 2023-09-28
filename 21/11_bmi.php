0a0a4ada0ea92c1a34c10aa55b3874b431a6ec12
y.asano <tennis6293@gmail.com>
2023-09-28 15:13:33 +0900

    update:21 11_bmi

--- 21/11_bmi.php
+++ 21/11_bmi.php
@@ -3,5 +3,10 @@
 $height = 175;
 $weigth = 70;
 
-echo '身長は' . $height . 'cmで体重は' . $weigth . 'kgのときのBMI値は' . number_format($weigth / (($height * 0.01)**2),2) . 'で、<br>' ;
-echo '適正体重は+'. number_format($weigth - (((($height * 0.01)**2) * 22)),2)  . 'kgです。';
+$bmi = $weigth / (($height * 0.01) ** 2);
+$proper = ($height * 0.01) ** 2 * 22;
+
+$difference = $weigth - $proper;
+
+echo '身長は' . $height . 'cmで体重は' . $weigth . 'kgのときのBMI値は' . number_format($bmi, 2) . 'で、<br>';
+echo '適正体重の' . ($difference > 0 ? '+' : '') . number_format($difference, 2) . 'kgです。';
