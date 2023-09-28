e5a220ff91a050494e7510fbf9299a94b94dba42
y.asano <tennis6293@gmail.com>
2023-09-28 15:20:54 +0900

    update:21 11_bmi コンスタントケース

--- 21/11_bmi.php
+++ 21/11_bmi.php
@@ -3,10 +3,10 @@
 $height = 175;
 $weigth = 70;
 
-$bmi = $weigth / (($height * 0.01) ** 2);
-$proper = ($height * 0.01) ** 2 * 22;
+$BMI = $weigth / (($height * 0.01) ** 2);
+$PROPER = ($height * 0.01) ** 2 * 22;
 
-$difference = $weigth - $proper;
+$DIFFERENCE = $weigth - $PROPER;
 
-echo '身長は' . $height . 'cmで体重は' . $weigth . 'kgのときのBMI値は' . number_format($bmi, 2) . 'で、<br>';
-echo '適正体重の' . ($difference > 0 ? '+' : '') . number_format($difference, 2) . 'kgです。';
+echo '身長は' . $height . 'cmで体重は' . $weigth . 'kgのときのBMI値は' . number_format($BMI, 2) . 'で、<br>';
+echo '適正体重の' . ($DIFFERENCE > 0 ? '+' : '') . number_format($DIFFERENCE, 2) . 'kgです。';
