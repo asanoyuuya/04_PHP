f81d636a5aeb08c71704d01f60f16725dbc85a50
y.asano <tennis6293@gmail.com>
2023-09-28 16:04:20 +0900

    add:21 12_magic

---
+++ 21/12_magic.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>ここは<?= __LINE__ ?>行目です。<br>パスは<?= __FILE__ ?>です。</P>
+</body>
+</html>
