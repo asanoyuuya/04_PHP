63fa569aaac9f3299289e2fc5d75397980b4a750
y.asano <tennis6293@gmail.com>
2023-09-28 17:07:12 +0900

    fix:21 12_magic

--- 21/12_magic.php
+++ 21/12_magic.php
@@ -1,16 +1,2 @@
 <?php
-$line = __LINE__;
-$file = __FILE__;
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <P>ここは<?= $line ?>行目です。<br>パスは<?= $file ?>です。</P>
-</body>
-</html>
+echo 'ここは' . __LINE__ . '行目です。<br>パスは' . __FILE__ . 'です。';
