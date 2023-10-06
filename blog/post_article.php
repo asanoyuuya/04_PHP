c82c8bd6210d70e4e05346d138a12aa64938ca56
y.asano <tennis6293@gmail.com>
2023-10-06 16:58:13 +0900

    update:7 post_article

--- blog/post_article.php
+++ blog/post_article.php
@@ -3,73 +3,61 @@
 require_once(dirname(__FILE__) . '/db.inc.php');
 require_once(dirname(__FILE__) . '/util.inc.php');
 
-$category_id = '';
-$title       = '';
-$article     = '';
-$isValidated = false;
+try {
+	$pdo         = dbConnect();
+	$category_id = '';
+	$title       = '';
+	$article     = '';
+	$isValidated = false;
 
-PDO::ERRMODE_EXCEPTION;
-$pdo = dbConnect();
+	if (!empty($_POST)) {
+		$category_id = $_POST['category'];
+		$title       = $_POST['title'];
+		$article     = $_POST['article'];
+		$isValidated = true;
 
-if (!empty($_POST)) {
-	$category_id = $_POST['category'];
-	$title       = $_POST['title'];
-	$article     = $_POST['article'];
-	$isValidated = true;
-	
-	
-	if ($title === '' || preg_match('/^(\s|　)+$/u', $title)) {
-		$titleError  = 'タイトルを入力してください';
-		$isValidated = false;
-	}
+		if ($title === '' || preg_match('/^(\s|　)+$/u', $title)) {
+			$titleError  = 'タイトルを入力してください';
+			$isValidated = false;
+		}
 
-	if ($article === '' || preg_match('/^(\s|　)+$/u', $article)) {
-		$articleError = '記事を入力してください';
-		$isValidated  = false;
-	} elseif (mb_strlen($article, 'utf8') > 100) {
-		$articleError = 'タイトルは100文字以内で入力してください';
-		$isValidated  = false;
-	}
+		if ($article === '' || preg_match('/^(\s|　)+$/u', $article)) {
+			$articleError = '記事を入力してください';
+			$isValidated  = false;
+		} elseif (mb_strlen($article, 'utf8') > 100) {
+			$articleError = 'タイトルは100文字以内で入力してください';
+			$isValidated  = false;
+		}
 
-	if ($isValidated == true ) {
-		try {
-			
+		if ($isValidated == true) {
 			$stmt = $pdo->prepare(
-				'INSERT INTO articles (category_id, title, article) VALUES (:category_id, :title, :article)');
-			
-			$stmt->bindValue(':category_id', (int)$category_id, PDO::PARAM_INT);
+				'INSERT INTO articles (category_id, title, article) VALUES (:category_id, :title, :article)'
+			);
+
+			$stmt->bindValue(':category_id', (int) $category_id, PDO::PARAM_INT);
 			$stmt->bindValue(':title', $title, PDO::PARAM_STR);
 			$stmt->bindValue(':article', $article, PDO::PARAM_STR);
 			$stmt->execute();
-			
-			$sql = $pdo->query('SELECT * FROM categories WHERE id =' . $category_id . '');
-			$c = $sql->fetch();
-			$category = $c['name'];
-			
-		} catch (PDOException $e) {
-			header('Content-Type: text/plain; charset=UTF-8', true, 500);
-			exit($e->getMessage());
-		}
-		
-	}
-}
 
-try {
-	$stmt = $pdo->query('SELECT * FROM categories');
+			$sql      = $pdo->query('SELECT * FROM categories WHERE id =' . $category_id . '');
+			$c        = $sql->fetch();
+			$category = $c['name'];
+		}
+	}
+
+	$stmt       = $pdo->query('SELECT * FROM categories');
 	$categories = $stmt->fetchAll();
+
 } catch (PDOException $e) {
 	header('Content-Type: text/plain; charset=UTF-8', true, 500);
 	exit($e->getMessage());
 }
 
 
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-	
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -85,57 +73,57 @@ try {
 
 		<section class="postform">
 			<p class="right"><a href="articles.php">記事の一覧に戻る</a></p>
-			<?php if($isValidated == true): ?>
-			<p>以下の内容で記事を保存しました。</p>
-			<table>
-				<tr>
-					<th>カテゴリ</th>
-					<td><?= $category ?></td>
-				</tr>
-				<tr>
-					<th>タイトル</th>
-					<td><?= $title ?></td>
-				</tr>
-				<tr>
-					<th>記事</th>
-					<td><?= $article ?></td>
-				</tr>
-			</table>
-			<?php else: ?>
-			<p>記事を入力し、送信ボタンを押してください。</p>
-			<table>
-					<form action="" method="post" novalidate>
+			<?php if ($isValidated == true): ?>
+				<p>以下の内容で記事を保存しました。</p>
+				<table>
 					<tr>
 						<th>カテゴリ</th>
-						<td>
-							<select name="category">
-								<?php foreach ($categories as $categorie): ?>
-										<option value="<?= $categorie['id'] ?>"><?= $categorie['name'] ?></option>
-								<?php endforeach; ?>
-							</select>
-						</td>
+						<td><?= h($category) ?></td>
 					</tr>
 					<tr>
 						<th>タイトル</th>
-						<td>
-							<input type="text" name="title" size="60" value="<?= h($title) ?>">
-							<?php if(isset($titleError)): ?>
-							<span class="error"><?= $titleError ?></span>
-							<?php endif; ?>
-						</td>
+						<td><?= h($title) ?></td>
 					</tr>
 					<tr>
 						<th>記事</th>
-						<td>
-							<textarea name="article" cols="60" rows="5"></textarea>
-							<?php if(isset($articleError)): ?>
-							<span class="error"><?= $articleError ?></span>
-							<?php endif; ?>
-						</td>
+						<td><?= nl2br(h($article)) ?></td>
+					</tr>
+				</table>
+			<?php else: ?>
+				<p>記事を入力し、送信ボタンを押してください。</p>
+				<table>
+						<form action="" method="post" novalidate>
+						<tr>
+							<th>カテゴリ</th>
+							<td>
+								<select name="category">
+									<?php foreach ($categories as $categorie): ?>
+											<option <?= $category_id == $categorie['id'] ? 'selected' : '' ?> value="<?= $categorie['id'] ?>"><?= $categorie['name'] ?></option>
+									<?php endforeach; ?>
+								</select>
+							</td>
 						</tr>
-					</table>
-					<p><input type="submit" value="送信"></p>
-				</form>
+						<tr>
+							<th>タイトル</th>
+							<td>
+								<input type="text" name="title" size="60" value="<?= h($title) ?>">
+								<?php if (isset($titleError)): ?>
+									<span class="error"><?= $titleError ?></span>
+								<?php endif; ?>
+							</td>
+						</tr>
+						<tr>
+							<th>記事</th>
+							<td>
+								<textarea name="article" cols="60" rows="5"></textarea>
+								<?php if (isset($articleError)): ?>
+									<span class="error"><?= $articleError ?></span>
+								<?php endif; ?>
+							</td>
+							</tr>
+						</table>
+						<p><input type="submit" value="送信"></p>
+					</form>
 			<?php endif; ?>
 			<p><a href="post_article.php">続けて投稿する</a></p>
 	</div>
